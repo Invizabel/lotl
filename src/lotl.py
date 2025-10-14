88bf289fb9a6a993b4041e79f0cef3d36e96af9d
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-10-13 17:23:10 -0700

    Add files via upload

--- src/lotl.py
+++ src/lotl.py
@@ -26,13 +26,6 @@ class lotl:
                 break
         return hits
 
-    def diff(self):
-        hits = []
-        self.data.reverse()
-        for i in range(len(self.data)-1):
-            hits.append(self.data[i] - self.data[i+1])
-        return hits
-
     def flatten(self):
         new_data = self.data
         if self.nth == -1:
@@ -97,4 +90,3 @@ class lotl:
         for i in self.data:
             hits.append((i - mean) / stdev)
         return hits
-
